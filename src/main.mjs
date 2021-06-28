36417b783c0fb250a45a51de8d9ea7c08e37cc44
ubermanu <manu@paca.happy-dev.fr>
2021-06-28 14:23:45 +0200

    add bulma prefers dark

--- src/main.mjs
+++ src/main.mjs
@@ -1,5 +1,6 @@
 import Flock from './Flock.svelte'
 import 'bulma/css/bulma.css'
+import 'bulma-prefers-dark/css/bulma-prefers-dark.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 export default new Flock({
