181268bfeec8167c9afdf4d9e7ed82272f005610
ubermanu <manu@paca.happy-dev.fr>
2021-06-28 20:38:11 +0200

    add a mention to show if the repo has been updated

--- src/Card.svelte
+++ src/Card.svelte
@@ -1,5 +1,17 @@
 <script>
   export let repository
+
+  /**
+   * Returns TRUE if the repo has been updated the last month
+   * @param updatedAt
+   * @return {boolean}
+   */
+  function isRecentlyUpdated(updatedAt) {
+    const now = new Date()
+    const updated = new Date(updatedAt)
+    const diff = now.getTime() - updated.getTime()
+    return diff <= (3600 * 24 * 30) * 1000
+  }
 </script>
 
 <div class="card">
@@ -7,6 +19,11 @@
     <a href="{repository.url}" target="_blank"><strong>{repository.nameWithOwner}</strong></a>
     <p class="card-description pt-1">{repository.description}</p>
     <div class="card-information pt-1">
+      {#if isRecentlyUpdated(repository.updatedAt)}
+        <small class="has-text-success mr-auto" title="Updated the last 30 days">
+          <i class="fa fa-arrow-up" aria-hidden="true"></i>
+        </small>
+      {/if}
       {#if repository.stargazerCount}
         <small><i class="fas fa-star"></i> {repository.stargazerCount}</small>
       {/if}
@@ -25,8 +42,10 @@
     }
 
     .card-information {
+        display: flex;
         margin-top: auto;
-        text-align: right;
+        justify-content: flex-end;
+        align-items: flex-end;
     }
 
     .card-information small + small {
